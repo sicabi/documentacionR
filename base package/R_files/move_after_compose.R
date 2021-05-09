1a4448c206a15ecc779a9e0fbb5485dac05875ee
sicabi <sicabi.cruz@gmail.com>
2021-05-09 22:23:13 +0300

    raw() {base}

--- base package/R_files/move_after_compose.R
+++ base package/R_files/move_after_compose.R
@@ -3,25 +3,21 @@ move_after_compose <- function(from, to) {
   if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive = TRUE)
   file.rename(from = from,  to = to)
 }
+delete_aux_files <- function(dir = character()) {
+  unlink(x = paste0(dir, grep(pattern = ".*tex",
+                                  x = dir(dir), 
+                                  value = TRUE, invert = TRUE)))
+}
 
-unlink(x = "base package/pdf_files/raw.pdf")
-  move_after_compose(from = "base package/tex_files/raw.pdf",
-               to = "base package/pdf_files/raw.pdf")
 
-  dir_tex <- "base package/tex_files/"
-  unlink(x = paste0(dir_tex, grep(pattern = ".*gz",
-                x = dir("base package/tex_files/"), 
-                value = TRUE)))
-  unlink(x = paste0(dir_tex, grep(pattern = ".*log",
-                                  x = dir("base package/tex_files/"), 
-                                  value = TRUE)))
-  unlink(x = paste0(dir_tex, grep(pattern = ".*toc",
-                                  x = dir("base package/tex_files/"), 
-                                  value = TRUE)))
-  unlink(x = paste0(dir_tex, grep(pattern = ".*aux",
-                                  x = dir("base package/tex_files/"), 
-                                  value = TRUE)))
-  unlink(x = paste0(dir_tex, grep(pattern = ".*out",
-                                  x = dir("base package/tex_files/"), 
-                                  value = TRUE)))
+file.name <- "raw.pdf"
+tex.dir <- "base package/tex_files/"
+pdf.dir <- "base package/pdf_files/"
+from.path <- paste0(tex.dir, file.name)
+to.path <- paste0(pdf.dir, file.name)
+
+unlink(x = to.path)
+move_after_compose(from = from.path,to = to.path)
+delete_aux_files(dir = tex.dir)
+
 
