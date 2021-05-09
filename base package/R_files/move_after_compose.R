02388892a583cb091a00dc9f69f5cc1a0e16ec0b
sicabi <sicabi.cruz@gmail.com>
2021-05-09 17:27:42 +0300

    raw() {base} translation

--- base package/R_files/move_after_compose.R
+++ base package/R_files/move_after_compose.R
@@ -4,9 +4,9 @@ move_after_compose <- function(from, to) {
   file.rename(from = from,  to = to)
 }
 
-unlink(x = "base package/pdf_files/all_names.pdf")
-  move_after_compose(from = "base package/tex_files/all_names.pdf",
-               to = "base package/pdf_files/all_names.pdf")
+unlink(x = "base package/pdf_files/raw.pdf")
+  move_after_compose(from = "base package/tex_files/raw.pdf",
+               to = "base package/pdf_files/raw.pdf")
 
   dir_tex <- "base package/tex_files/"
   unlink(x = paste0(dir_tex, grep(pattern = ".*gz",
